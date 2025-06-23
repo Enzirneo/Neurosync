2dad3fca09708993c1086a27ae70816feac3f61e
enzirneo <enzo.bispo22@gmail.com>
2025-06-23 15:46:28 -0300

    Correção ortografica

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Neusosync Headset</h1>
+        <h1>NeuroSync Headset</h1>
         <p>Maximize sua concentração com a mais avançada estimulação<br></br> neural do mercado.</p>
         <div className="hero-buttons">
           <a href="#features" className="hero-button">VER DEMONSTRAÇÃO</a>
